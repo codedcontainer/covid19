d81768fd625a02d199f36fa351928d1c26e0458d
jutt <jutt@iu.edu>
2020-04-21 11:34:25 -0400

    update selenium DOM targets

--- IN.js
+++ IN.js
@@ -4,8 +4,8 @@ const { Builder, By } = require('selenium-webdriver')
 async function getStats() {
   let driver = await new Builder().forBrowser('chrome').build();
   await driver.get("https://www.coronavirus.in.gov/map-test/test.htm");
-  const totalPositiveCases = await driver.findElement(By.id("total_pt")).getText();
-  const totalDeaths = await driver.findElement(By.id("deaths")).getText();
+  const totalPositiveCases = await driver.findElement(By.css(".m-stats-cards-card-wrapper:nth-child(4) .h1")).getText()
+  const totalDeaths = await driver.findElement(By.css(".m-stats-cards-card-wrapper:nth-child(5) .h1")).getText()
   driver.close();
   return {
     'IN': {
