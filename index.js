35d3c80541a430805d739d889a934382b6f2c593
Jason Utt <jdmagic21@gmail.com>
2020-04-25 19:13:18 -0400

    moved loop to promise then

--- index.js
+++ index.js
@@ -8,14 +8,17 @@ const async = require('async');
 (async () => {
     spreadsheet.loadInfo().then((doc) => {
         US.setStats(doc, 0); 
-        IN.setStats(doc, 953330401);
+        //IN.setStats(doc, 953330401);
 
         var i = 0;
         function myLoop() {
             setTimeout(() => {
-                api.insertMultiple(doc, sheets[i].state, sheets[i].sheetId)
-                i++;
-                if (i <= sheets.length -1) myLoop();
+                api.insertMultiple(doc, sheets[i].state, sheets[i].sheetId).then(()=>{
+                    console.log(sheets[i].state); 
+                    i++
+                    if (i <= sheets.length -1) myLoop();
+                }); 
+               
             }, 10000);
         }
         myLoop();
